bf09273d679ab493df7595b3708de96ff7f39c8a
aallawi <allawi100@yahoo.com>
2023-09-08 06:43:22 +0300

    Update map

--- src/Pages/ContactPage/ContactPage.js
+++ src/Pages/ContactPage/ContactPage.js
@@ -10,8 +10,13 @@ function ContactPage() {
   return (
     <div className="ContactPage bg_black">
       <Basic pageName={"Contact"} url={"main"} />
-      <div className="map bg_black">
-        <img src={images.map} alt="map" />
+      <div className="section_padding bg_black">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d75202.57170327388!2d31.324399456892877!3d30.037027298970248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2seg!4v1694143795460!5m2!1sen!2seg"
+          width="100%"
+          height="450"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
       <Suggestion />
       <Footer />
