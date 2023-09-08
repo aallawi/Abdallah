98ad2d0d59abf450aa952ebba6ae90345c8328e3
aallawi <allawi100@yahoo.com>
2023-09-08 06:53:00 +0300

    Upadte contact page css

--- src/Pages/ContactPage/ContactPage.js
+++ src/Pages/ContactPage/ContactPage.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 import Basic from "../../components/Basic/Basic";
-import { images } from "../../constants";
 
-import "./ContactPage.css";
 import { Footer, Suggestion } from "../../container";
 
 function ContactPage() {
@@ -15,6 +13,7 @@ function ContactPage() {
           src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d75202.57170327388!2d31.324399456892877!3d30.037027298970248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2seg!4v1694143795460!5m2!1sen!2seg"
           width="100%"
           height="450"
+          style={{ border: "4px solid #DCCA87" }}
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
